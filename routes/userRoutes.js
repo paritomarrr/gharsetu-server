992b6949c54554f5f20d560e2791261fe0596a31
Keshav Malik <keshav2021csds116@abesit.edu.in>
2024-11-29 12:49:37 +0530

    Added Seller profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import {userTest, saveUserDetails} from '../controllers/userControllers.js'
+import { getSellerProfile, getSellerName } from '../controllers/sellerController.js'
 
 const userRoutes = express.Router()
 
 userRoutes.get('/', userTest)
 userRoutes.post('/saveUserDetails', saveUserDetails)
+userRoutes.post('/getSellerProfile', getSellerProfile)
+userRoutes.post('/getSellerName', getSellerName)
 
 export default userRoutes 
